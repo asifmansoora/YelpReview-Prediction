d9432cfbe58f89cd1831212d51330b1fd49814ee
Rajnish Kumar <rajnish.garg27@gmail.com>
2013-04-23 01:24:19 -0500

    #Code update (Complete Run)

--- Code/prediction_example.py
+++ Code/prediction_example.py
@@ -16,6 +16,7 @@ from nltk.stem import WordNetLemmatizer
 from nltk.collocations import* 
 from FormatText import * #external function file
 from ModelFunctions import *
+from FormatFeatures import *
 
 wordNetStemmer = WordNetLemmatizer()
 
@@ -38,11 +39,14 @@ sValidatinFile = 'consolidated_yelp_validation_data.csv'
 
 n_neighbors = 10
 
-Regressor_Name = 'svr'
+#Regressor_Name = 'svr'
 #Regressor_Name = 'nusvr'
 #Regressor_Name = 'Gausian'
-#Regressor_Name = 'Nearest_Neighbors_uniform'
+Regressor_Name = 'Nearest_Neighbors_uniform'
 #Regressor_Name = 'Nearest_Neighbors_distance'
+
+
+IgnoreColumn = 'total_reviews_written_by_user'
  
 #################################################################################
 # LOADING TRAINING DATA
@@ -57,27 +61,30 @@ X_train = []
 reviewData = []
 iCount = 0
 
+print "LOADING THE TRAINING DATA"
 for line in trainfile:
-    splitted = line.rstrip().split(',')
-    
+    splitted = line.rstrip().split(',')    
     wordList =  clean_review(splitted[1])
     #print wordList
     
     #wordList =  getPOSList(clean_review(splitted[1]),Noun=True)
     splitted[1] = len(wordList)
     
-    # print word_stemming(wordList,wordNetStemmer)
-    """
-    if(iCount == 10):
+    
+    #wordList =  word_stemming(wordList[0],wordNetStemmer)
+    
+
+   
+    if(iCount == 100000):
         break
     iCount = iCount +1
-    continue
-    """
+    #continue
+    
     label = int(splitted[0])
     features = []
     for item in splitted[1:30]:
         if(item == 'NULL'):
-            features.append(item)
+            features.append(np.nan)
         else:
             features.append(float(item))
 
@@ -87,11 +94,26 @@ for line in trainfile:
     X_train.append(features)
     
 trainfile.close()
-
+print "\t Data loaded succesfull"
 
 #After we have converted all of our feature into number we fill convert it into np format
 y_train = np.array(y_train)
 X_train = np.array(X_train)
+
+
+# Delete the unrequired columns from the Dataset
+try:
+    iIndex = header.index(IgnoreColumn)-1
+    X_train = np.delete(X_train,iIndex,1)
+    print "\t Deleted the unrequired columns"
+except:
+    print "\t No column matched for removing"
+
+
+
+#Handle NULL(nan) values
+X_train[:,2] = fill_avg(column(X_train,2))
+print "\t Missing Values: Added mean values in the missing data"
 
  
 ###########################
@@ -106,8 +128,8 @@ X_train = np.array(X_train)
  
 
 
-#X_train = transform_features_standardize(X_train) 
-
+X_train = transform_features_standardize(X_train) 
+print "\t Transformed the features"
 
 ###############################################################################
 ### SELECT THE MODEL TO RUN
@@ -118,12 +140,12 @@ X_train = np.array(X_train)
 
 model = SelectModel(Regressor_Name)
 model.fit(X_train,y_train)
-
+print "\t Fit the model: ",Regressor_Name
 
 ###############################################################################
 # READING VALIDATION DATA
 ###############################################################################
-
+print "LOADING THE VALIDATION DATA"
 validationfile = open(sValidatinFile)
 headerValidation = validationfile.next().rstrip().split(',')
  
@@ -141,7 +163,7 @@ for line in validationfile:
     features = []
     for item in splitted[1:30]:
         if(item == 'NULL'):
-            features.append(item)
+            features.append(np.nan)
         else:
             features.append(float(item))
     #features = [float(item) for item in splitted[1:]]
@@ -151,7 +173,7 @@ for line in validationfile:
     
 validationfile.close()
 
-
+print "\t Data loaded succesfull"
 
 
 #After we have converted all of our feature into number we fill convert it into np format
@@ -159,9 +181,28 @@ y_validation = np.array(y_validation)
 X_validation = np.array(X_validation)
 
 
+
+# Delete the unrequired columns from the Dataset
+try:
+    iIndex = header.index(IgnoreColumn)-1
+    X_validation = np.delete(X_validation,iIndex,1)
+    print "\t Deleted the unrequired columns" 
+except:
+    print "\t No column matched for removing"
+
+
+#Handle NULL(nan) values
+
+X_validation[:,2] = fill_avg(column(X_validation,2))
+
+
+print "\t Missing Values: Added mean values in the missing data"   
+
+
+
 # compute result on the validation data 
 y_predict = model.predict(X_validation)
-
+print "\n Predicting the model result: \n"
 
 
 
@@ -175,7 +216,7 @@ print "Mean Square Error on Validation data is: ",mean_squared_error(y_validatio
 print "R square error is: ",r2_score(y_validation, y_predict)                           
 print "Root Mean Square Log Error: ", calculate_RMSLE(y_validation, y_predict) 
 
-1/0
+
 #Cross-Validation: evaluating estimator performance
 if(bCrossValidation):
     print "Cross Validation: RMSLE is: ",RunCrossValidation(X_train,y_train,Regressor_Name,kFold)," after ",kFold," iteration"
@@ -185,21 +226,57 @@ if(bCrossValidation):
 ###############################################################################
 # READING TEST DATA
 ###############################################################################
- 
+print "LOAD THE TEST DATA..."
 testfile = open(sTestingFile)
-#ignore the test header
-testfile.next()
- 
+#Getting test header
+testheader = testfile.next().rstrip().split(',')
+
 X_test = []
+review_id =[]
+iIndex = testheader.index('review')
 
 for line in testfile:
     splitted = line.rstrip().split(',')
-    features = [float(item) for item in splitted]
-    X_test.append(features)
+    #here review is at 0 id
+    wordList =  clean_review(splitted[iIndex])
+    #print wordList
     
+    #wordList =  getPOSList(clean_review(splitted[1]),Noun=True)
+    splitted[iIndex] = len(wordList)
+    #wordList =  word_stemming(wordList[0],wordNetStemmer)
+    features = []
+
+    #Adding review Id for test file
+    review_id.append(splitted[29])
+    for item in splitted[0:29]:
+        if(item == 'NULL'):
+            features.append(np.nan)
+        else:
+            features.append(float(item))
+
+    
+    X_test.append(features)
+ 
 testfile.close()
  
 X_test = np.array(X_test)
+
+
+# Delete the unrequired columns from the Dataset
+try:
+    iIndex = testheader.index(IgnoreColumn)
+    X_test = np.delete(X_test,iIndex,1)
+    print "\t Deleted the unrequired columns: ",IgnoreColumn,
+except:
+    print "\t No column matched for removing"
+
+
+
+#Handle NULL(nan) values
+X_test[:,2] = fill_avg(column(X_test,2))
+print "\t Missing Values: Added mean values in the missing data"
+
+ 
 
 ###############################################################################
 # TRANSFORMING THE FEATURES 
@@ -214,14 +291,15 @@ X_test = transform_features_standardize(X_test)
 ###############################################################################
                 
 p_test = model.predict(X_test)
-                
-###############################################################################
+
+##############################################################################
 # WRITING SUBMISSION FILE
 ###############################################################################
-predfile = open('predictions.csv','w+')
- 
-print >>predfile,','.join(["review_id","class"])
-for line in np.concatenate((X_test,p_test),axis=1):
-    print >>predfile, ','.join([str(item) for item in line])
- 
-predfile.close()
+prediction = open("prediction.txt","w")
+for i in p_test:
+    prediction.write((str(i) +"\n"))
+prediction.close()
+
+
+	
+
